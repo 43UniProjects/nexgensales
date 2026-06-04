197bbabaff8770ffa636a4f4bbac0a9f08053bab
Minuraka Iroshan <minurakairoshan@gmail.com>
2026-06-04 20:45:11 +0530

    style: standardize HomeView UI to match global dark theme and custom controls

--- Views/HomeView.xaml.cs
+++ Views/HomeView.xaml.cs
@@ -1,26 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NextGenSales.Views
 {
-    /// <summary>
-    /// Interaction logic for HomeView.xaml
-    /// </summary>
     public partial class HomeView : Window
     {
         public HomeView()
         {
             InitializeComponent();
+        }
+
+        // Title bar window dragging
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        // Minimize Button
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        // Maximize/Restore Button
+        private void BtnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+        // Close Button
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        // Top Right Goto Process Button
+        private void BtnGotoProcess_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Navigate to the next view (e.g., ExportView or AnalyticsView)
+            MessageBox.Show("Navigating to Process...", "Navigation", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
