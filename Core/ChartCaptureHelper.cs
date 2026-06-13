f26a5461233e5d026949788ad8cf46198fe83663
Janindu Malshan <janindumalshan50@gmail.com>
2026-06-13 09:34:01 +0530

    feat: added a feature to save .png files of charts to identify issues with the charts capturing

--- Core/ChartCaptureHelper.cs
+++ Core/ChartCaptureHelper.cs
@@ -36,6 +36,12 @@ namespace NexGenSales.Core
             using var stream = new MemoryStream();
             encoder.Save(stream);
 
+            // DEBUG: Save converted chart to reports folder
+            var debugDir = "reports";
+            if (!Directory.Exists(debugDir))
+                Directory.CreateDirectory(debugDir);
+            File.WriteAllBytes(Path.Combine(debugDir, $"chart_{System.Guid.NewGuid()}.png"), stream.ToArray());
+
             return stream.ToArray();
         }
     }
